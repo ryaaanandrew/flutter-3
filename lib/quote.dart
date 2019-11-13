466f280f02e3f29b5db4d047e1c4e84925373a22
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-11-12 22:10:15 -0500

    inital commit

---
+++ lib/quote.dart
@@ -0,0 +1,22 @@
+class Quote {
+  String text;
+  String author;
+
+  Quote({ this.text, this.author });
+
+}
+
+
+//class Quote {
+//
+//  String text;
+//  String author;
+//
+//  old
+//  Quote(String text, String author) {
+//    this.text = text;
+//    this.author = author;
+//  }
+//
+//}
+//
